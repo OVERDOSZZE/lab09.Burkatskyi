f419448761e9c0b27aef85320908ac4fd2620f6e
OVERDOSZZE <stasbur10@gmail.com>
2026-04-30 00:16:40 +0200

    Add files via upload

---
+++ IKM752aDSG.Lab09.Burkatskyi.cpp
@@ -0,0 +1,44 @@
+﻿
+
+#include <iostream>
+
+  
+
+    int main()
+    {
+        double a[3][3]; 
+        double sum[3] = { 0 }, min, max;
+
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++) {
+              std::cin >> a[i][j];
+                sum[i] += a[i][j];
+            }
+
+        min = max = a[0][0];
+
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++) {
+                if (a[i][j] < min) min = a[i][j];
+                if (a[i][j] > max) max = a[i][j];
+            }
+
+        std::cout << max * min;
+        std::cout << max - min;
+
+        for (int i = 0; i < 3; i++)
+         std::cout << sum[i] << " ";
+
+       
+    }
+
+
+
+
+
+
+
+
+
+
+
